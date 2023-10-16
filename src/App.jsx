a23734292264459e20576a4bf4dc32819e64a792
ajps208 <ajithps208@gmail.com>
2023-10-16 12:20:02 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './Components/Counter';
+
+function App() {
+  return (
+    <>
+     <h1 className='text-center mt-5'>Counter Application</h1>
+     <Counter/>
+    </>
+  );
+}
+
+export default App;
